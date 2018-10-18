54946c99b9cb46ffdf222d9697a8dafb171bbb70
TheGuyFromIndia <31314482+TheGuyFromIndia@users.noreply.github.com>
2018-10-18 11:28:17 +0530

    initial commit

---
+++ app/src/main/java/com/catchthe/android/swoosh/WelcomeAcvtivity.kt
@@ -0,0 +1,12 @@
+dpackage com.catchthe.android.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WelcomeAcvtivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_acvtivity)
+    }
+}
